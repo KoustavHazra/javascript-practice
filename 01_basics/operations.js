a3f3bf2966ff09d7add91674829f0420da5b8f7f
Koustav Hazra <36538480+KoustavHazra@users.noreply.github.com>
2024-03-04 07:01:17 +0530

    Create operations.js

---
+++ 01_basics/operations.js
@@ -0,0 +1,48 @@
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);  // "12"
+// console.log("1" + 2 + 3);  // "123"
+// console.log("1" - 2 + 3);  // 1 - 2 = -1 --> -1 + 3 = 2 (number)
+// console.log(1 + 2 + "3")  // 1 + 2 = 3 --> 3 + "3" = "33"
+
+// console.log(+true);  // 1
+// console.log(-true);  // -1
+// console.log(true+); // throws error
+// console.log(false-); // throws erroe
+
+// console.log(+false);  // 0
+// console.log(-false);  // -0
+
+// console.log(+"");  // 0
+// console.log(""+); // throws error
+
+
+let gameCounter = 100;
+++gameCounter;
+// console.log(gameCounter);  // prefix
+
+gameCounter++;
+// console.log(gameCounter);  // postfix
+
+
